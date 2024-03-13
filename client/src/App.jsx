704a6d20e2e06d5b16081ac182f19a9d5f669258
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-14 00:52:53 +0530

    routing and multiple packages installed

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {lazy} from "react"
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Groups = lazy(() => import("./pages/Groups"));
+const Chat = lazy(() => import("./pages/Chat"));
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
